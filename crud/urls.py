6b0d82c72ba6599aa8cbb076f33cdcbe954cdda9
ADA <arpon.alexander@gmail.com>
2020-05-26 00:26:41 +0800

    fifth commit

--- crud/urls.py
+++ crud/urls.py
@@ -19,8 +19,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('crud_register.urls')),
+    path('api/', include('crud_register.urls')),
     path('crud/', schema_view),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
